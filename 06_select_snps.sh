49aa56236bb4f5eb7da81306aa474baab7453402
Yangzhen Li <liyz@ysfri.ac.cn>
2025-09-06 02:16:22 +0800

    Create 06_select_snps.sh

---
+++ 06_select_snps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script extracts SNP variants from the multi-sample VCF using GATK SelectVariants.
+
+# Define input and output files (update paths accordingly)
+input_vcf="/path/to/sole_WGS_pipeline/vcf/all_samples.vcf.gz"
+output_vcf="/path/to/sole_WGS_pipeline/vcf/raw_snps.vcf.gz"
+
+# Run GATK SelectVariants to select only SNPs from the VCF
+gatk SelectVariants \
+  -select-type SNP \
+  -V "$input_vcf" \
+  -O "$output_vcf"
