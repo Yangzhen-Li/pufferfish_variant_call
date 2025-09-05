2da16cba625ac1d019d86c9a4d911957f8ef9bff
Yangzhen Li <liyz@ysfri.ac.cn>
2025-09-06 02:19:26 +0800

    Update 07_filter_snps.sh

--- 07_filter_snps.sh
+++ 07_filter_snps.sh
@@ -3,12 +3,12 @@
 # Variants failing filters will be marked in the output VCF's FILTER column.
 
 # Define input and output paths (update paths accordingly)
-input_vcf="/path/to/sole_WGS_pipeline/vcf/raw_snps.vcf.gz"
-output_vcf="/path/to/sole_WGS_pipeline/vcf/snps_hardfiltered.vcf.gz"
+input_vcf="/path/to/vcf/raw_snps.vcf.gz"
+output_vcf="/path/to/vcf/snps_hardfiltered.vcf.gz"
 
 # Run GATK VariantFiltration to apply hard filter criteria to SNPs
 nohup gatk VariantFiltration \
     -V "${input_vcf}" \
     --filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
     --filter-name "Filter" \
-    -O "${output_vcf}" > "/path/to/sole_WGS_pipeline/vcf/hardfilter.log" 2>&1 &
+    -O "${output_vcf}" > "/path/to/vcf/hardfilter.log" 2>&1 &
